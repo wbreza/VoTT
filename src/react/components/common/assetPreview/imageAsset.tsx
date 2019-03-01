41f8f58ede9e39567c0a83a3624a576f14fe55b0
Phil Cherner <pcherner@mit.edu>
2019-03-01 09:24:59 -0800

    Begin error catching

--- src/react/components/common/assetPreview/imageAsset.tsx
+++ src/react/components/common/assetPreview/imageAsset.tsx
@@ -6,14 +6,39 @@ import { IAssetProps } from "./assetPreview";
  */
 export class ImageAsset extends React.Component<IAssetProps> {
     private image: React.RefObject<HTMLImageElement> = React.createRef();
+    private hasError: boolean = false;
 
     public render() {
-        return (<img ref={this.image} src={this.props.asset.path} onLoad={this.onLoad} />);
+        return (
+            <img ref={this.image}
+                src={this.props.asset.path}
+                onLoad={this.onLoad}
+                onError={this.onError}
+                alt={"Could not load image"}/>
+            // <div>
+            //     {!this.hasError &&
+            //         <img ref={this.image}
+            //             src={this.props.asset.path}
+            //             onLoad={this.onLoad}
+            //             onError={this.onError}
+            //             alt={"Could not load image"}/>
+            //     }
+            // </div>
+        );
     }
 
     private onLoad = () => {
         if (this.props.onLoaded) {
             this.props.onLoaded(this.image.current);
+            this.hasError = false;
         }
+    }
+
+    private onError = (e: React.SyntheticEvent<HTMLImageElement>) => {
+        console.log(e);
+        e.preventDefault();
+        e.stopPropagation();
+        // this.image.current.src = "http://www.rangerwoodperiyar.com/images/joomlart/demo/default.jpg";
+        this.hasError = true;
     }
 }
